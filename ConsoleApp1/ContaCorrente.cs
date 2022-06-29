c56aaa98b2964a266eb060804f1606b4940973e4
Atillio Pinno Fetter <atilliopf@hotmail.com>
2022-06-29 08:28:44 -0300

    Add project files.

---
+++ ConsoleApp1/ContaCorrente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    internal class ContaCorrente
+    {
+        private string titular;
+        private string conta;
+        private int numeroAgencia;
+        private string nomeAgencia;
+        private double saldo;
+
+
+        public ContaCorrente(string conta, int numeroAgencia, string nomeAgencia, string titular = null, double saldo = 0 )
+        {
+            this.numeroAgencia = numeroAgencia;
+            this.saldo = saldo;
+            this.nomeAgencia = nomeAgencia;
+            this.conta = conta;
+            this.titular = titular;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(valor<0)
+                return false;
+            if (saldo < valor)
+                return false;
+            else
+                saldo-=valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {   
+            if (valor>0)                
+            saldo = saldo + valor;
+
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+                return false;
+            if (valor < 0)
+                return false;
+            else
+            {
+                saldo = saldo - valor; ;
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using bytebank;
+
+Console.WriteLine("hello");
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Atillio";
+conta1.conta = "0asd-a";
+conta1.numeroAgencia = 12;
+conta1.nomeAgencia = "asdasd";
+conta1.saldo = 9884034.09;
+
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número Agencia: " + conta1.numeroAgencia);
+Console.WriteLine("Nome Agencia: " + conta1.nomeAgencia);
+Console.WriteLine("Saldo: "+ conta1.saldo);
+
+bool saque = conta1.Sacar(2000);
+Console.WriteLine("Saque realizado? "+(saque?"Sim":"Não"));
+Console.WriteLine("Saldo: " + conta1.saldo);
